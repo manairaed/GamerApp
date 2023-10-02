55ff5bc07f0d9d005bc952e79f4bc92bc1217692
manairaed <manairaed1998@gmail.com>
2023-10-02 23:21:01 +0100

    Game Layouts

--- app/src/main/java/com/example/gamerapp/MainActivity.kt
+++ app/src/main/java/com/example/gamerapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.gamerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gamerapp.databinding.LoginBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : LoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
